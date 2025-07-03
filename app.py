86aba7b6784f127d5cc839c625663c39c2c71bfb
Shreyash <tupeshreyash24@gmail.com>
2025-07-03 19:08:29 +0530

    changes update

--- app.py
+++ app.py
@@ -6,22 +6,15 @@ st.title('Insurance Premium Prediction App')
 # Input fields
 age = st.number_input('Enter your age:', min_value=1, max_value=120, value=25)
 
-# gender = st.radio('Select gender:', ['male', 'female'])
-# gender = 0 if gender == 'male' else 1
-
 gender = st.radio('Select gender:', ['male', 'female'], horizontal=True)
 gender = 0 if gender == 'male' else 1
-
-smoker = st.radio('Do you smoke?', ['yes', 'no'], horizontal=True)
-smoker = 1 if smoker == 'yes' else 0
-
 
 bmi = st.number_input('Enter BMI:', min_value=10.0, max_value=60.0, value=25.0)
 
 children = st.number_input('Enter number of children:', min_value=0, max_value=10, value=0)
 
-# smoker = st.radio('Do you smoke?', ['yes', 'no'])
-# smoker = 1 if smoker == 'yes' else 0
+smoker = st.radio('Do you smoke?', ['yes', 'no'], horizontal=True)
+smoker = 1 if smoker == 'yes' else 0
 
 # Prepare input
 X_test = [[age, gender, bmi, children, smoker]]
